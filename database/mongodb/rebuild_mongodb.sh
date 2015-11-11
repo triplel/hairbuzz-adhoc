91a4e84b66a4830511b55034f2acbc2005e1fff1
Liang <liliang.li@yahoo.com>
2015-11-10 23:00:53 -0500

    added customer collection script to  bash shell

--- database/mongodb/rebuild_mongodb.sh
+++ database/mongodb/rebuild_mongodb.sh
@@ -3,4 +3,5 @@
 #todo - set unique indexes
 python database/mongodb/rebuild_venue_collection.py --venue resources/test_data/venues.psv
 python database/mongodb/rebuild_stylist_collection.py --stylist resources/test_data/stylists.psv --venue resources/test_data/venues.psv
+python database/mongodb/rebuild_customer_collection.py --customer resources/test_data/customers.psv
 echo "MongoDB dev database rebuild completed!"
